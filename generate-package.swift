d1c69c1c6849e6af98a8971d76cb87e726ea0bdf
Doug <6060466+pixlwave@users.noreply.github.com>
2021-06-19 16:52:48 +0100

    Update generate-package.swift

--- generate-package.swift
+++ generate-package.swift
@@ -3,13 +3,14 @@ import Foundation
 func packageString(url: String, checksum: String) -> String {
     """
     // swift-tools-version:5.3
-
     import PackageDescription
-
     let package = Package(
         name: "MatrixSDK",
         products: [
             .library(name: "MatrixSDK", targets: ["MatrixSDK"])
+        ],
+        dependencies: [
+            .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", .exact("\(swiftyBeaverVersion())"))
         ],
         targets: [
             .binaryTarget(name: "MatrixSDK", url: "\(url)", checksum: "\(checksum)")
@@ -19,9 +20,36 @@ func packageString(url: String, checksum: String) -> String {
     """
 }
 
+func swiftyBeaverVersion() -> String {
+    // load Podfile.lock
+    let lockFileURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+        .appendingPathComponent("matrix-ios-sdk")
+        .appendingPathComponent("Podfile.lock")
+    
+    guard let lockFile = try? String(contentsOf: lockFileURL) else {
+        fatalError("Unable to read Podfile.lock")
+    }
+    
+    // find the first occurance of SwiftyBeaver
+    // this should be in the PODS: section in the format
+    // - SwiftyBeaver (x.x.x)
+    guard let swiftyBeaverLock = lockFile.components(separatedBy: .newlines).first(where: { $0.contains("- SwiftyBeaver") }) else {
+        fatalError("Unable to find SwiftyBeaver in Podfile.lock")
+    }
+    
+    // match the version in the brackets
+    guard let range = swiftyBeaverLock.range(of: #"(?<=\().*?(?=\))"#, options: .regularExpression) else {
+        fatalError("Unable to detect SwiftyBeaver's version from Podfile.lock")
+    }
+    
+    let version = swiftyBeaverLock[range]
+    print("SwiftyBeaver dependency version: \(version)\n")
+    
+    return String(version)
+}
+
 guard CommandLine.arguments.count == 3 else {
-    print("Usage: swiftc generate-package.swift url checksum")
-    exit(-1)
+    fatalError("Usage: swiftc generate-package.swift url checksum")
 }
 
 print("Generating binary package")
